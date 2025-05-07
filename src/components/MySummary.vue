f001c52d8a7e20ccf7070fc98b6d3eb97ce7ef1d
thaingocphu0803 <167735819+thaingocphu0803@users.noreply.github.com>
2025-05-07 14:32:13 +0700

    get url param -done

--- src/components/MySummary.vue
+++ src/components/MySummary.vue
@@ -1,6 +1,8 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import { usePositionStore } from '@/stores/getPosition'
+
+const positionStore = usePositionStore()
+const position = positionStore.position
 </script>
 
 <template>
@@ -9,14 +11,14 @@
   >
     <div class="basis-1/3 flex justify-center">
       <img
-        src="../assets/logo.svg"
+        src="../assets/images/logo.webp"
         alt=""
         class="object-cover object-center w-[150px] h-[150px] bg-white rounded-full border-5 border-amber-500 hover:border-amber-600"
       />
     </div>
     <div class="basis-2/3 flex flex-col justify-center">
       <div class="text-2xl font-bold">Paragraph example 1: about Bali</div>
-      <div class="text-lg font-medium text-amber-600">Paragraph example 1: about Bali</div>
+      <div class="text-lg font-medium text-amber-600">{{ position }}</div>
       <div class="font-medium text-sm text-wrap mt-3">
         Below are a series of poorly constructed paragraphs and possible solutions. Put yourself in
         the place of a teacher. Criticise the structure of each paragraph and suggest how it might
